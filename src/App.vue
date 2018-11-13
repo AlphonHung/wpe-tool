426ba2edc4bdd2869804254678880edab5ab7a5d
DesmondHung <desmond.hung@isapsolution.com>
2018-11-13 14:07:45 +0800

    todolist basic

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,118 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id='app'>
+    <!-- <img alt='Vue logo' src='./assets/logo.png'> -->
+    <!-- <HelloWorld msg=''/> -->
+    <div class="block">
+      <toDoList v-bind:items="toDoList" />
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script lang='ts'>
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import toDoList from "./components/todo-list/todo-list.vue";
 
 export default Vue.extend({
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld,
+    toDoList
   },
+  data() {
+    return {
+      toDoList: [
+        {
+          title: "Test A",
+          details: [
+            { key: "事項1", value: "測試功能" },
+            { key: "事項2", value: "Debug" },
+            { key: "自訂事項", value: "Wryyyyyy" }
+          ]
+        },
+        {
+          title: "Test B",
+          details: [
+            { key: "B-1", value: "I am test 4" },
+            { key: "B-1", value: "I am test 4" },
+            { key: "B-1", value: "I am test 4" }
+          ]
+        }
+      ]
+    };
+  }
 });
 </script>
 
-<style lang="less">
+<style lang='less'>
+/*清除內外框margin*/
+html,
+body,
+div,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+hr,
+p,
+blockquote,
+dl,
+dt,
+dd,
+ul,
+ol,
+li,
+pre,
+form,
+fieldset,
+object,
+code,
+em,
+span,
+var,
+legend,
+button,
+input,
+textarea,
+th,
+td,
+a,
+img,
+header,
+footer,
+nav,
+aside,
+audio,
+datalist,
+section {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  overflow-y: auto;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-image: url("./assets/background/background.jpg");
+  .block {
+    height: 100%;
+    width: 300px;
+    margin: 10px 5px;
+    display: inline-block;
+    overflow-y: scroll;
+    background-color: rgba(64, 64, 64, 0.6);
+  }
 }
 </style>
---
+++ src/components/todo-list/todo-list.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class='todo-list'>
+        <h1 class='header'>To Do List</h1>
+        <button class='editButton' v-on:click="clickEdit()">Edit</button>
+        <ul>
+            <li v-for='item in items' :key='item.id'>
+                <toDoListItem v-bind:item='item' v-bind:isEditMode='flag.isEditMode' />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang='ts'>
+import Vue from "vue";
+import toDoListItem from "./todo-list-item.vue";
+
+export default Vue.extend({
+  name: "todo-list",
+  components: {
+    toDoListItem
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      flag: {
+        isEditMode: false
+      }
+    };
+  },
+  methods: {
+    clickEdit() {
+      this.flag.isEditMode = !this.flag.isEditMode;
+    }
+  }
+});
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped lang='less'>
+.todo-list {
+  position: relative;
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  .header {
+    text-align: center;
+    color: white;
+  }
+  .editButton {
+    position: absolute;
+    right: 14px;
+    top: 9px;
+    border: 1px solid white;
+    border-radius: 16px;
+    padding: 5px 10px;
+    color: white;
+    background-color: transparent;
+    transition: background-color;
+    transition-duration: 300;
+    &:hover {
+      background-color: #e6e6e6;
+      color: black;
+    }
+  }
+}
+</style>
